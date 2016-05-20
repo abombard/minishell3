b9c4681779371912bc1f8265a649045e995bb5ef
abombard <arthbab@gmail.com>
2016-05-20 17:40:50 +0200

    ref

--- srcs/main.c
+++ srcs/main.c
@@ -192,6 +192,23 @@ static bool minishell__termios_raw(const int fd)
 	termios_new.c_cc[VMIN] = 1;
 	termios_new.c_cc[VTIME] = 0;
 
+	struct serial_struct serinfo;
+	if (ioctl(fd, TIOCGSERIAL, &serinfo) < 0)
+		FATAL("ioctl() TIOCGSERIAL failed %s", "");
+	serinfo.flags &= ~ASYNC_SPD_MASK;
+	serinfo.flags |= ASYNC_SPD_CUST;
+	serinfo.custom_divisor = (serinfo.baud_rate + (rate / 2)) / rate;
+	if (ioctl(fd, TIOCSSERIAL, &serinfo) < 0)
+		FATAL("ioctl() TIOCSERIAL failed %s", "");	
+	if (ioctl(fd, TIOCGSERIAL, &serinfo) < 0)
+		FATAL("ioctl() TIOCGSERIAL %s", "");	
+	if (serinfo.custom_divisor * rate != serinfo.baud_base)
+	{
+		FATAL("actual baudrate is %d / %d = %f",
+		      serinfo.baud_base, serinfo.custom_divisor,
+		      (float)serinfo.baud_base / serinfo.custom_divisor);
+	}
+
 	/* set terminal in raw mode */
 	if (tcsetattr(fd, TCSAFLUSH, &termios_new) != 0)
 		FATAL("tcsetattr() failed to initialize %s\r", "");
